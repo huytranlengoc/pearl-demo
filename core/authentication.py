dc643abcd988e210f353c2695921aaa24dd4a548
huytranlengoc <huytranlengoc@gmail.com>
2023-04-02 00:21:37 +0700

    implement authentication

--- core/authentication.py
+++ core/authentication.py
@@ -1,5 +1,6 @@
 import jwt
 import datetime
+from rest_framework import exceptions
 
 def create_access_token(id):
     return jwt.encode({
@@ -7,6 +8,13 @@ def create_access_token(id):
         'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
         'iat': datetime.datetime.utcnow()
     }, 'access_secret', algorithm='HS256')
+
+def decode_access_token(token):
+    try:
+        payload = jwt.decode(token, 'access_secret', algorithms=['HS256'])
+        return payload['user_id']
+    except:
+        raise exceptions.AuthenticationFailed('unauthenticated')
 
 def create_refresh_token(id):
     return jwt.encode({
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import RegisterApiView, LoginAPIView
+from .views import RegisterApiView, LoginAPIView, UserAPIView
 
 urlpatterns = [
     path('register', RegisterApiView.as_view() ),
     path('login', LoginAPIView.as_view() ),
+    path('user', UserAPIView.as_view() ),
 
 ]
